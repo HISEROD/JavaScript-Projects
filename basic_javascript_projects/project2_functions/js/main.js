dd303ab25c04e49ed5788e7ca230809de2d45a13
HISEROD <hisebeast@gmail.com>
2022-03-17 22:01:24 -0500

    fixed file structure and created project2_functions

---
+++ basic_javascript_projects/project2_functions/js/main.js
@@ -0,0 +1,18 @@
+var awesome = (1 + 1 / 144) ** 144, expression = "(1 + 1 / 144) ** 144 = "; // approximate Euler's number using one of Jacob Bernoulli's formulae
+window.alert(expression + awesome);                                         // alert the user of the output of the preceding expression
+document.write("Euler's number has been successfully approximated.")        // inform the user of success
+
+function My_First_Function() {              // Defining a function and naming it
+    var str = "This is the button text!";   // Defining a variable and giving it
+                                            // a string value
+    str = str.fontcolor("lightgray");       // make the text light gray
+    document.getElementById("Button_Text").innerHTML = str; // Putting the value
+                                            // of the variable into the HTML elementFromPoint
+                                            // with the "Button_Text" id
+}
+
+function My_Second_Function() {
+    window.alert(                           // Alert the user that they are supposed to know everything.
+        "You hovered over the button intending to click it instead of just pressing Enter! Come on, dude! I autofocused it just for you and you didn't even acknowledge it. smh"
+    )
+}
