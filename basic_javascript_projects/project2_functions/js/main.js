395bef4914c50792df70ef08588c284b37066ce8
HISEROD <hisebeast@gmail.com>
2022-03-17 22:36:26 -0500

    modified project2

--- basic_javascript_projects/project2_functions/js/main.js
+++ basic_javascript_projects/project2_functions/js/main.js
@@ -1,11 +1,12 @@
-var awesome = (1 + 1 / 144) ** 144, expression = "(1 + 1 / 144) ** 144 = "; // approximate Euler's number using one of Jacob Bernoulli's formulae
-window.alert(expression + awesome);                                         // alert the user of the output of the preceding expression
-document.write("Euler's number has been successfully approximated.")        // inform the user of success
+var awesome = toString((1 + 1 / 144) ** 144), expression = "(1 + 1 / 144) ** 144 = ";   // approximate Euler's number using one of Jacob Bernoulli's formulae
+expression += awesome;                                                                  // add awesome to expression by concatenation
+window.alert(expression);                                                               // alert the user of the output of the preceding expression
+document.write("Euler's number has been successfully approximated.");                   // inform the user of success
 
 function My_First_Function() {              // Defining a function and naming it
-    var str = "This is the button text!";   // Defining a variable and giving it
+    var str = "I hope you pressed Enter instead of clicking!";   // Defining a variable and giving it
                                             // a string value
-    str = str.fontcolor("lightgray");       // make the text light gray
+    str = str.fontcolor("red");             // make the text light gray
     document.getElementById("Button_Text").innerHTML = str; // Putting the value
                                             // of the variable into the HTML elementFromPoint
                                             // with the "Button_Text" id
@@ -14,5 +15,13 @@ function My_First_Function() {              // Defining a function and naming it
 function My_Second_Function() {
     window.alert(                           // Alert the user that they are supposed to know everything.
         "You hovered over the button intending to click it instead of just pressing Enter! Come on, dude! I autofocused it just for you and you didn't even acknowledge it. smh"
-    )
+    );
+}
+
+var hue = 120;
+
+function Yet_Another_Function() {
+    // set the color of the paragraph every time it is hovered ↓
+    document.getElementById("paragraph").style.color = `hsl(${window["hue"]}, 100%, 50%)`;
+    window["hue"] += 60; // increment the hue by 60 degrees
 }
