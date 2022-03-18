1015bbc475d7520f91e2176dd3aa7ef9ec0af1ac
HISEROD <hisebeast@gmail.com>
2022-03-17 21:09:55 -0500

    created project1

---
+++ basic_javascript_projects/js/main.js
@@ -0,0 +1,12 @@
+var awesome = (1 + 1 / 144) ** 144, expression = "(1 + 1 / 144) ** 144 = "; // approximate Euler's number using one of Jacob Bernoulli's formulae
+window.alert(expression + awesome);                                         // alert the user of the output of the preceding expression
+document.write("Euler's number has been successfully approximated.")        // inform the user of success
+
+function My_First_Function() {              // Defining a function and naming it
+    var str = "This is the button text!";   // Defining a variable and giving it
+                                            // a string value
+    str = str.fontcolor("lightgray");       // make the text light gray
+    document.getElementById("Button_Text").innerHTML = str; // Putting the value
+                                            // of the variable into the HTML elementFromPoint
+                                            // with the "Button_Text" id
+}
