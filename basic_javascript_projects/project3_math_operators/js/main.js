bc7b674a8e887cd47c06e99449e2ee745a87d6d6
HISEROD <hisebeast@gmail.com>
2022-03-18 23:38:15 -0500

    fixed double semicolon

--- basic_javascript_projects/project3_math_operators/js/main.js
+++ basic_javascript_projects/project3_math_operators/js/main.js
@@ -58,7 +58,7 @@ function tau_function() {
     document.getElementById("math").innerHTML = `
         &#120591; (spelled and pronounced: tau, approximately ${tau.toPrecision(3)}) is a better circle constant than &#120587; (pi, approximately ${Math.PI.toPrecision(3)}). This is because &#120591; represents the full circumference of the ubiquitous unit circle rather than just half of it, like &#120587;. Because of this, fractions of rotations are much easier to mentally compute, for example, three fifths of a turn (or one full rotation) in terms of &#120587; is (6&nbsp;/&nbsp;5)&nbsp;*&nbsp;&#120587;, but with &#120591; it can be expressed as (3&nbsp;/&nbsp;5)&nbsp;*&nbsp;&#120591;. No extra simplification steps! Simply multiply the fraction by &#120591;.<br><br>
 
-        As you can see, with &#120591;; you no longer have to deal with continually multiplying and dividing by 2 when working with radian angle measures.<br>
+        As you can see, with &#120591; you no longer have to deal with continually multiplying and dividing by 2 when working with radian angle measures.<br>
         More intuitive eh?
     `;
 }
